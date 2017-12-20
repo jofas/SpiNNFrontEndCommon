234614dd62c01dd5b11654d0895f1fbb2530e5b5
Luca Peres <luca.peres@icloud.com>
2017-12-20 17:34:57 +0000

    Moved C++ code in the correct position and script updated

---
+++ c_common/models/data_speed_up_packet_gatherer/c_code/pybind_install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone --recursive https://github.com/bast/pybind11-demo
+cd pybind11-demo
+mkdir build
+rm -f CMakeLists.txt
+cp ../CMakeLists.txt ./
+rm -f example.cpp
+cd build
+cmake ..
+make
+cp ./host_data_receiver.so ../../../../../../spinn_front_end_common/utility_models/
+cd ../..
+rm -rf pybind11-demo
