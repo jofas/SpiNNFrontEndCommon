c1a6b72cdda72efc649bb7d0d8480590ab129840
rjames91 <robert.james@manchester.ac.uk>
2019-02-18 10:19:41 +0000

    an group vertices to allow for routing at large scales of SpiNNak-Ear current version has single layer of 16 atom AN group vertices with intention for more layers to exist

--- spinn_front_end_common/interface/interface_functions/edge_to_n_keys_mapper.py
+++ spinn_front_end_common/interface/interface_functions/edge_to_n_keys_mapper.py
@@ -6,6 +6,7 @@ from spinn_front_end_common.abstract_models import (
 from spinn_front_end_common.utilities.exceptions import ConfigurationException
 from spinnak_ear.spinnakear_vertex import SpiNNakEarVertex
 from spinnak_ear.IHCAN_vertex import IHCANVertex
+from spinnak_ear.AN_group_vertex import ANGroupVertex
 
 class EdgeToNKeysMapper(object):
     """ Works out the number of keys needed for each edge.
@@ -68,9 +69,12 @@ class EdgeToNKeysMapper(object):
         vertex = graph_mapper.get_application_vertex(
             partition.pre_vertex)
         if isinstance(vertex,SpiNNakEarVertex):
-            if isinstance(partition.pre_vertex,IHCANVertex):
-                ihc_ids = [i for i, name in enumerate(vertex._mv_index_list) if name == 'ihc']
-                vertex = vertex._mv_list[ihc_ids[vertex_slice.lo_atom/2]]
+            # if isinstance(partition.pre_vertex,IHCANVertex):
+            #     ihc_ids = [i for i, name in enumerate(vertex._mv_index_list) if name == 'ihc']
+            #     vertex = vertex._mv_list[ihc_ids[vertex_slice.lo_atom/2]]
+            if isinstance(partition.pre_vertex,ANGroupVertex):
+                group_ids = [i for i, name in enumerate(vertex._mv_index_list) if name == 'group']
+                vertex = vertex._mv_list[group_ids[vertex_slice.lo_atom/vertex._MAX_N_ATOMS_PER_CORE]]
             else:
                 vertex = vertex._mv_list[vertex_slice.lo_atom]
 
